9dcd3242f2eb2a253a794deb46552d4710566f5f
xpyct01 <kopitko94@gmail.com>
2024-01-06 14:43:08 +0200

    Modified kubeflow.tf

--- code/infrastructure/terraform/kubeflow.tf
+++ code/infrastructure/terraform/kubeflow.tf
@@ -3,6 +3,10 @@ module "kubeflow_manifests" {
   source = "github.com/kubeflow/manifests"
 }
 
+output "kubeflow_manifests_path" {
+  value = path.module
+}
+
 resource "null_resource" "kubeflow" {
   depends_on = [module.kubeflow_manifests, kubernetes_namespace.namespace]
   provisioner "local-exec" {
@@ -11,31 +15,31 @@ resource "null_resource" "kubeflow" {
       kubectl apply -k common/istio-1-17/istio-namespace/base
       kubectl apply -k common/istio-1-17/istio-install/base
     EOT
-    working_dir = module.kubeflow_manifests.source
+    working_dir = module.kubeflow_manifests.kubeflow_manifests_path
   }
 
   provisioner "local-exec" {
     command = "kubectl apply -k common/istio-1-17/kubeflow-istio-resources/base"
-    working_dir = module.kubeflow_manifests.source
+    working_dir = module.kubeflow_manifests.kubeflow_manifests_path
   }
 
   provisioner "local-exec" {
     command = "kubectl apply -k common/kubeflow-roles/base"
-    working_dir = module.kubeflow_manifests.source
+    working_dir = module.kubeflow_manifests.kubeflow_manifests_path
   }
 
   provisioner "local-exec" {
     command = "kubectl apply -k common/oidc-client/oidc-authservice/base"
-    working_dir = module.kubeflow_manifests.source
+    working_dir = module.kubeflow_manifests.kubeflow_manifests_path
   }
 
   provisioner "local-exec" {
     command = "kubectl apply -k apps/profiles/upstream/overlays/kubeflow"
-    working_dir = module.kubeflow_manifests.source
+    working_dir = module.kubeflow_manifests.kubeflow_manifests_path
   }
 
   provisioner "local-exec" {
     command = "kubectl apply -k apps/pipeline/upstream/env/cert-manager/platform-agnostic-multi-user"
-    working_dir = module.kubeflow_manifests.source
+    working_dir = module.kubeflow_manifests.kubeflow_manifests_path
   }
 }
