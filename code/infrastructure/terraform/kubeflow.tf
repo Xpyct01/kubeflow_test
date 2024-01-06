8eaba0f87d3c437de26fc938ed3f48ee4f8a66b3
xpyct01 <kopitko94@gmail.com>
2024-01-06 16:11:40 +0200

    Modified kubeflow.tf

--- code/infrastructure/terraform/kubeflow.tf
+++ code/infrastructure/terraform/kubeflow.tf
@@ -20,8 +20,8 @@ resource "null_resource" "kubeflow" {
   provisioner "local-exec" {
     when = destroy
     command = <<-EOT
+      cd ./.terraform/modules/kubeflow_manifests
       while ! kubectl delete -k example; do echo "Retrying to apply resources"; sleep 10; done
     EOT
-    working_dir = var.kubeflow_repo_path
   }
 }
