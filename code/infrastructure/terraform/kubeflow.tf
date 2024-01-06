96bfdbd12bb74ac5cb300ef32ac95fcb3cc6bc57
xpyct01 <kopitko94@gmail.com>
2024-01-06 14:07:49 +0200

    Added kubeflow.tf

---
+++ code/infrastructure/terraform/kubeflow.tf
@@ -0,0 +1,41 @@
+# set up kubeflow
+module "kubeflow_manifests" {
+  source = "github.com/kubeflow/manifests"
+}
+
+resource "null_resource" "kubeflow" {
+  depends_on = [module.kubeflow_manifests, kubernetes_namespace.namespace]
+  provisioner "local-exec" {
+    command = <<-EOT
+      kubectl apply -k common/istio-1-17/istio-crds/base
+      kubectl apply -k common/istio-1-17/istio-namespace/base
+      kubectl apply -k common/istio-1-17/istio-install/base
+    EOT
+    working_dir = "manifests"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -k common/istio-1-17/kubeflow-istio-resources/base"
+    working_dir = "manifests"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -k common/kubeflow-roles/base"
+    working_dir = "manifests"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -k common/oidc-client/oidc-authservice/base"
+    working_dir = "manifests"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -k apps/profiles/upstream/overlays/kubeflow"
+    working_dir = "manifests"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -k apps/pipeline/upstream/env/cert-manager/platform-agnostic-multi-user"
+    working_dir = "manifests"
+  }
+}
