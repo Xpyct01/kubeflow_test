4e5944042024851b867cf9b8d7482c0e3941ff0c
xpyct01 <kopitko94@gmail.com>
2024-01-06 15:55:31 +0200

    Modified kubeflow.tf

--- code/infrastructure/terraform/kubeflow.tf
+++ code/infrastructure/terraform/kubeflow.tf
@@ -12,7 +12,7 @@ resource "null_resource" "kubeflow" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      while ! kustomize build example | kubectl apply -f -; do echo "Retrying to apply resources"; sleep 10; done
+      while ! kubectl apply -k example; do echo "Retrying to apply resources"; sleep 10; done
     EOT
     working_dir = var.kubeflow_repo_path
   }
