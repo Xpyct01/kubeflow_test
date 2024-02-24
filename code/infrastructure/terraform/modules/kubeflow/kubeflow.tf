eb9238fdbedf6243f4323dab0e7ffc958ca76e0a
xpyct01 <kopitko94@gmail.com>
2024-02-24 16:58:12 +0200

    Modified terraform files

--- code/infrastructure/terraform/modules/kubeflow/kubeflow.tf
+++ code/infrastructure/terraform/modules/kubeflow/kubeflow.tf
@@ -19,7 +19,10 @@ resource "null_resource" "kubeflow" {
   }
 
   provisioner "local-exec" {
+    command = "cd ${var.kubeflow_manifests_path}"
+  }
+
+  provisioner "local-exec" {
     command = "while ! kubectl apply -k vanilla; do echo 'Retrying to apply resources'; sleep 10; done"
-    working_dir = var.kubeflow_manifests_path
   }
 }
